93b8614ae226d8ec8a2f3186cf9ee97f1140c384
Carlos de la Orden <cdelaorden@gmail.com>
2023-12-28 00:21:14 +0100

    day 7 part 2

--- src/days/7/index.test.ts
+++ src/days/7/index.test.ts
@@ -1,4 +1,4 @@
-import { mapOfOcurrences, parseInput } from '.'
+import { addJoker, mapOfOcurrences, parseInput, sortByMostCommon } from '.'
 
 describe('Camel Cards', () => {
   describe('parseInput', () => {
@@ -17,10 +17,45 @@ describe('Camel Cards', () => {
 
 describe('mapOfOcurrences', () => {
   it('returns an array of distinct string chars sorted by higher appearence', () => {
-    expect(mapOfOcurrences('aaa')).toHaveLength(1)
-    const result = mapOfOcurrences('AAABBBCCD')
+    expect(sortByMostCommon(mapOfOcurrences('aaa'))).toHaveLength(1)
+    const result = sortByMostCommon(mapOfOcurrences('AAABBBCCD'))
     expect(result).toHaveLength(4)
     expect(result.at(0)?.at(0)).toBe('A')
     expect(result.at(3)?.at(0)).toBe('D')
+  })
+})
+
+describe('addJoker', () => {
+  it('does nothing if J not present', () => {
+    const result = addJoker({
+      hand: 'KQK12345',
+      bid: 0,
+    })
+    expect(result.hand).toBe('KQK12345')
+  })
+  it('adds the best possible card if present', () => {
+    const result = addJoker({
+      bid: 0,
+      hand: 'KK134J',
+    })
+    expect(result.hand).toEqual('KK134K')
+  })
+
+  it('takes into account J being the most common', () => {
+    const cases = [
+      ['QQJJJ', 'QQQQQ'],
+      ['76QJJ', '76QQQ'],
+      ['JK6JK', 'KK6KK'],
+      ['J43JJ', '44344'],
+      ['JJJJJ', 'JJJJJ'],
+    ]
+    cases.forEach((c) => {
+      expect(
+        addJoker({
+          hand: c.at(0) as string,
+          bid: 1,
+        }).hand
+      ).toBe(c.at(1))
+    })
   })
 })
--- src/days/7/index.ts
+++ src/days/7/index.ts
@@ -3,6 +3,14 @@ import assert from 'node:assert'
 import { map, pipe, reduce, sort } from 'remeda'
 
 const ALPHABET = 'AKQJT98765432'
+const ALPHABET_WITH_JOKER = 'AKQT98765432J'
+
+type ParsedHand = {
+  hand: string
+  bid: number
+  // for joker replacement, keep original here for draws
+  original?: string
+}
 
 export const parseInput = (input: string): ParsedHand[] =>
   pipe(
@@ -14,10 +22,6 @@ export const parseInput = (input: string): ParsedHand[] =>
       bid: Number(parts.at(1)?.trim()) ?? 0,
     }))
   )
-type ParsedHand = {
-  hand: string
-  bid: number
-}
 
 export const mapOfOcurrences = (s: string) =>
   pipe(
@@ -26,19 +30,18 @@ export const mapOfOcurrences = (s: string) =>
     reduce((acc, c) => {
       acc.set(c, (acc.get(c) ?? 0) + 1)
       return acc
-    }, new Map<string, number>()),
-    sortByMostCommon
+    }, new Map<string, number>())
   )
 
-const sortByMostCommon = (m: Map<string, number>) =>
+export const sortByMostCommon = (m: Map<string, number>) =>
   pipe(
     Array.from(m.entries()),
     sort((p1, p2) => p2[1] - p1[1])
   )
 
-const compareHandPoints = (h1: ParsedHand, h2: ParsedHand): 0 | 1 | -1 => {
-  const c1 = mapOfOcurrences(h1.hand)
-  const c2 = mapOfOcurrences(h2.hand)
+const compareHandPoints = (h1: ParsedHand, h2: ParsedHand) => {
+  const c1 = sortByMostCommon(mapOfOcurrences(h1.hand))
+  const c2 = sortByMostCommon(mapOfOcurrences(h2.hand))
   for (let i = 0; i < Math.min(c1.length, c2.length); i++) {
     const x1 = c1.at(i)?.at(1)
     const x2 = c2.at(i)?.at(1)
@@ -52,16 +55,50 @@ const compareHandPoints = (h1: ParsedHand, h2: ParsedHand): 0 | 1 | -1 => {
   return 0
 }
 
+const findBestMostCommon = (
+  entries: [string, number][],
+  priorities = ALPHABET_WITH_JOKER
+) =>
+  entries.reduce(
+    (acc, entry) => {
+      if (entry[1] > acc[1]) return entry
+      if (entry[1] < acc[1]) return acc
+      if (priorities.indexOf(entry[0]) < priorities.indexOf(acc[0])) {
+        return entry
+      } else {
+        return acc
+      }
+    },
+    ['J', -1]
+  )
+
+export const addJoker = (hand: ParsedHand) => {
+  if (!hand.hand.includes('J')) return hand
+  const m = mapOfOcurrences(hand.hand.replaceAll('J', ''))
+  const sortedByMostCommon = sortByMostCommon(m)
+  const mostCommon = findBestMostCommon(sortedByMostCommon)
+  const result = { ...hand }
+  if (mostCommon) {
+    result.hand = result.hand.replaceAll('J', mostCommon[0])
+    result.original = hand.hand
+  }
+  return result
+}
+
 const compareHands = (
   h1: ParsedHand,
   h2: ParsedHand,
+  comparePoints = compareHandPoints,
   priorities = ALPHABET
 ) => {
-  const byPoints = compareHandPoints(h1, h2)
+  const byPoints = comparePoints(h1, h2)
   if (byPoints !== 0) return byPoints
+
   for (let i = 0; i < h1.hand.length; i++) {
-    const rank1 = priorities.indexOf(h1.hand[i] as string)
-    const rank2 = priorities.indexOf(h2.hand[i] as string)
+    const card1 = h1.original ? h1.original[i] : h1.hand[i]
+    const card2 = h2.original ? h2.original[i] : h2.hand[i]
+    const rank1 = priorities.indexOf(card1 as string)
+    const rank2 = priorities.indexOf(card2 as string)
     if (rank1 > rank2) {
       return -1
     }
@@ -88,4 +125,13 @@ export function day7PartOne(sample: string, input: string) {
 
 export function day7PartTwo(sample: string, input: string) {
   assert(sample && input, 'Bad input data')
+  const solution = pipe(
+    input,
+    parseInput,
+    map(addJoker),
+    sort((a, b) => compareHands(a, b, compareHandPoints, ALPHABET_WITH_JOKER)),
+    map.indexed(calculateHandValue),
+    sum
+  )
+  console.log('Part two: %d', solution)
 }
