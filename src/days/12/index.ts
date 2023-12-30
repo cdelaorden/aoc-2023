dfafb20087adbdcf9ea5867f24bae22a0bc0fa10
Carlos de la Orden <cdelaorden@gmail.com>
2023-12-30 01:54:05 +0100

    setup day 12

---
+++ src/days/12/index.ts
@@ -0,0 +1,10 @@
+import assert from 'assert'
+
+// https://adventofcode.com/2023/day/12
+export function day12PartOne(sample = '', input = '') {
+  assert(sample && input, 'Missing input data')
+}
+
+export function day12PartTwo(sample = '', input = '') {
+  assert(sample && input, 'Missing input data')
+}
